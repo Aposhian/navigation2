f7531328aee9fe0adb5ca7fd9830aac7a4677d9b
Adam Aposhian <aposhian.dev@gmail.com>
2022-02-23 01:21:13 -0500

    use integrated distance for lookahead distance
    (cherry picked from commit 6e4c6cd3a795faf29f977df7316b5d58d9c1f135)

--- nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
+++ nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
@@ -94,10 +94,16 @@ public:
 <<<<<<< HEAD
 =======
 
-  double findCuspWrapper(
-    const geometry_msgs::msg::PoseStamped & pose)
+  std::vector<geometry_msgs::msg::PoseStamped>::iterator findCuspWrapper()
   {
-    return findCusp(pose);
+    return findCusp();
+  }
+
+  double integratedDistanceToPoseWrapper(
+    const nav_msgs::msg::Path & path,
+    const std::vector<geometry_msgs::msg::PoseStamped>::iterator iterator)
+  {
+    return integratedDistanceToPose(path, iterator);
   }
 >>>>>>> 77c88bfd... rename findDirectionChange to findCusp
 };
@@ -169,13 +175,15 @@ TEST(RegulatedPurePursuitTest, findCusp)
   path.poses[2].pose.position.x = -1.0;
   path.poses[2].pose.position.y = -1.0;
   ctrl->setPlan(path);
-  auto rtn = ctrl->findCuspWrapper(pose);
+  auto cusp = ctrl->findCuspWrapper();
+  auto rtn = ctrl->integratedDistanceToPoseWrapper(path, cusp);
   EXPECT_EQ(rtn, sqrt(5.0));
 
   path.poses[2].pose.position.x = 3.0;
   path.poses[2].pose.position.y = 3.0;
   ctrl->setPlan(path);
-  rtn = ctrl->findCuspWrapper(pose);
+  cusp = ctrl->findCuspWrapper();
+  rtn = ctrl->integratedDistanceToPoseWrapper(path, cusp);
   EXPECT_EQ(rtn, std::numeric_limits<double>::max());
 }
 
